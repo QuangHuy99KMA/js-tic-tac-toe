77ce2e3c65e48bae1336778c08f0315d39432df3
Quang Huy <quanghuyhg8999@gmail.com>
2022-10-20 18:10:47 +0700

    init cell element list

--- js/main.js
+++ js/main.js
@@ -1,16 +1,55 @@
-import { getCellElementList, getCurrentTurnElement, getGameStatusElement, getCellElementAtIdx } from "./selectors.js";
+import { TURN } from "./constants.js";
+import {
+    getCellElementList,
+    getCurrentTurnElement,
+    getGameStatusElement,
+    getCellElementAtIdx
+} from "./selectors.js";
 
-console.log(getCellElementList());
-console.log(getCurrentTurnElement());
-console.log(getGameStatusElement());
-console.log(getCellElementAtIdx(4));
+// console.log(getCellElementList());
+// console.log(getCurrentTurnElement());
+// console.log(getGameStatusElement());
+// console.log(getCellElementAtIdx(4));
+
 
 /**
  * Global variables
  */
-let currentTurn = "cross";
+let currentTurn = TURN.CROSS;
 let isGameEnded = false;
 let cellValues = new Array(9).fill("");
+
+function toogleTurn() {
+    // toggle turn
+    currentTurn = currentTurn === TURN.CIRCLE ? TURN.CROSS : TURN.CIRCLE;
+
+    // update turn on DOM element
+    const curentTurnElemnt = getCurrentTurnElement();
+    if (curentTurnElemnt) {
+        curentTurnElemnt.classList.remove(TURN.CIRCLE, TURN.CROSS);
+        curentTurnElemnt.classList.add(currentTurn);
+    }
+}
+
+function handleCellClick(cell, index) {
+    const isClicked = cell.classList.contains(TURN.CIRCLE) || cell.classList.contains(TURN.CROSS);
+    if (isClicked) return;
+
+    // set selected cell
+    cell.classList.add(currentTurn);
+
+    //toggle turn 
+    toogleTurn();
+
+    // console.log('click', cell, index);
+}
+
+function initCellElementList() {
+    const cellElementList = getCellElementList();
+    cellElementList.forEach((cell, index) => {
+        cell.addEventListener("click", () => handleCellClick(cell, index));
+    })
+}
 
 /**
  * TODOs
@@ -27,3 +66,9 @@ let cellValues = new Array(9).fill("");
  * 4. On replay button click --> reset game to play again.
  *
  */
+
+(() => {
+    // bind click event for all li elements
+    initCellElementList();
+    // bind click event for replay button
+})()
